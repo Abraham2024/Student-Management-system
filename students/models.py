e06d299e90f8d4dd0d16009a709204f1c35d6574
Abraham2024 <ibrahimjunior08@gmail.com>
2024-01-12 11:25:18 +0100

    my_commit

--- students/models.py
+++ students/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Students (models.Model):
+    Student_name = models.PositiveIntegerField()
+    First_name = models.CharField(max_length = 50)
+    Last_name = models.CharField(max_length = 50)
+    Email = models.EmailField(max_length = 50)
+    Field_of_study = models.CharField(max_length = 50)
+    gpa = models.FloatField()
+
+    def __str__(self):
+        return f'student: {self.first_name} {self.last_name}'
+
--- students/views.py
+++ students/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Students
 
 # Create your views here.
 def index(request):
-    return render(request, 'students/index.html')
+    return render(request, 'students/index.html', {
+    'students': Students.objects.all()
+  })
